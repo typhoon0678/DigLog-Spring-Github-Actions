a61bb2e8eb3d948059bc9bffc2e0271bcf254e21
typhoon0678 <typhoon0678@gmail.com>
2025-01-23 15:03:42 +0900

    feat: 초기 세팅

---
+++ src/main/java/api/store/diglog/DiglogApplication.java
@@ -0,0 +1,13 @@
+package api.store.diglog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiglogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiglogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/api/store/diglog/DiglogApplicationTests.java
@@ -0,0 +1,13 @@
+package api.store.diglog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiglogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
