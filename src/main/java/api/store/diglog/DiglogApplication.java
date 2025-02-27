aebbe001b63006ecf7ab8a7d69d341be10334362
Frod90 <142146516+Frod90@users.noreply.github.com>
2025-02-27 11:45:17 +0900

    chore: 불필요한 import 제거

--- src/main/java/api/store/diglog/DiglogApplication.java
+++ src/main/java/api/store/diglog/DiglogApplication.java
@@ -2,13 +2,12 @@ package api.store.diglog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 public class DiglogApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(DiglogApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(DiglogApplication.class, args);
+	}
 
 }
