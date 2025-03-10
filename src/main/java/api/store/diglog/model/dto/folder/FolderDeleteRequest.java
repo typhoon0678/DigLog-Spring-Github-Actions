8b1507eb9d93da40e46ae1842707c71979c3e779
Frod90 <142146516+Frod90@users.noreply.github.com>
2025-03-11 05:43:07 +0900

    feat: Builder 생성자 추가

--- src/main/java/api/store/diglog/model/dto/folder/FolderDeleteRequest.java
+++ src/main/java/api/store/diglog/model/dto/folder/FolderDeleteRequest.java
@@ -2,10 +2,15 @@ package api.store.diglog.model.dto.folder;
 
 import java.util.UUID;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
 @Getter
 public class FolderDeleteRequest {
 
